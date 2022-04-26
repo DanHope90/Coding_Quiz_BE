701e27215c96def232d2b4d08048a8739fd6948c
Dan Hope <dplhope@gmail.com>
2022-04-26 14:35:19 +0100

    mongoose connction added (get connection failing)

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const questionsRouter = require("./routers/questionsRouter")
 
 const APP_PORT = 4000;
 
-app.get("/", function (req, res) {
-    res.send("Hello world!")
+//create environment variable for password ? Do we use .ENV file or nodemon.json? 
+mongoose.connect("mongodb+srv://Quiz_API_Mock:MLcpD4lOWmQUZL8c@cluster0.08qp2.mongodb.net/Cluster0?retryWrites=true&w=majority",);
+
+app.get("/", (req, res) => {
+    res.send("Hello world!!!")
 });
 
+app.get("/questions", questionsRouter);
+
+//maybe think of creating an environment variable for port connection for deployment
 app.listen(APP_PORT, () => {
     console.log(`Our App is now listening on http://localhost:${APP_PORT}`);
 });
