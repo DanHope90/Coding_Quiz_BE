00c817095ab4bf6a55bbe8b20aecffebb32deed0
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-03 13:20:17 +0100

    updated quizModel

--- back-end/src/models/quizModel.js
+++ back-end/src/models/quizModel.js
@@ -1,24 +1,15 @@
 const mongoose = require('mongoose');
 
 const quizSchema = new mongoose.Schema({
-  
-    quiz: String,
-      question: String,
-       answers: {
-        answer_a: String,
-        answer_b: String,
-        answer_c: String,
-        answer_d: String,
-      },
-      correct_answers: {
-        correct_answers_a: Boolean,
-        correct_answers_b: Boolean,
-        correct_answers_c: Boolean,
-        correct_answers_d: Boolean,
-      },  
-  },
-{
-    timestamps: true,
+  quiz: String,
+  question: String,
+  answers: [
+    {answer_a: String, isCorrect: Boolean},
+    {answer_b: String, isCorrect: Boolean},
+    {answer_c: String, isCorrect: Boolean},
+    {answer_d: String, isCorrect: Boolean},
+  ],
+  timestamps: true,
 });
 
 module.exports = mongoose.model('Quiz', quizSchema);
