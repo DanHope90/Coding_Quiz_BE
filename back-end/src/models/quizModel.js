3ebf9799e7ceeac69269b2deb311e6be8c1e50fc
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-03 15:53:00 +0100

    updated quiz model

--- back-end/src/models/quizModel.js
+++ back-end/src/models/quizModel.js
@@ -1,18 +1,67 @@
 const mongoose = require('mongoose');
 
+// const quizSchema = new mongoose.Schema({
+//   quiz: String,
+//   question: String,
+//   answers: [
+    // {answer_a: String, isCorrect: Boolean},
+    // {answer_b: String, isCorrect: Boolean},
+    // {answer_c: String, isCorrect: Boolean},
+    // {answer_d: String, isCorrect: Boolean},
+//   ],
+//   timestamps: true,
+// });
+
 const quizSchema = new mongoose.Schema({
   quiz: String,
-  question: String,
-  answers: [
-    {answer_a: String, isCorrect: Boolean},
-    {answer_b: String, isCorrect: Boolean},
-    {answer_c: String, isCorrect: Boolean},
-    {answer_d: String, isCorrect: Boolean},
-  ],
-},
-
-{
-  timestamps: true
-});
+  questions: [{
+    question_1: String,
+    answers_1: [
+      {answer_a: String, isCorrect: Boolean},
+      {answer_b: String, isCorrect: Boolean},
+      {answer_c: String, isCorrect: Boolean},
+      {answer_d: String, isCorrect: Boolean},
+    ]
+  },
+  {
+    question_2: String,
+    answers_2: [
+      {answer_a: String, isCorrect: Boolean},
+      {answer_b: String, isCorrect: Boolean},
+      {answer_c: String, isCorrect: Boolean},
+      {answer_d: String, isCorrect: Boolean},
+    ]
+  },
+  {
+    question_3: String,
+    answers_3: [
+      {answer_a: String, isCorrect: Boolean},
+      {answer_b: String, isCorrect: Boolean},
+      {answer_c: String, isCorrect: Boolean},
+      {answer_d: String, isCorrect: Boolean},
+    ]
+  },
+  {
+    question_4: String,
+    answers_4: [
+      {answer_a: String, isCorrect: Boolean},
+      {answer_b: String, isCorrect: Boolean},
+      {answer_c: String, isCorrect: Boolean},
+      {answer_d: String, isCorrect: Boolean},
+    ]
+  },
+  {
+    question_5: String,
+    answers_5: [
+      {answer_a: String, isCorrect: Boolean},
+      {answer_b: String, isCorrect: Boolean},
+      {answer_c: String, isCorrect: Boolean},
+      {answer_d: String, isCorrect: Boolean},
+    ]
+  },
+],
+}, 
+    {timestamps: true},
+)
 
 module.exports = mongoose.model('Quiz', quizSchema);
