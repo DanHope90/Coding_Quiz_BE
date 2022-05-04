2b28596c5446711701db27b655377dcc41ab4978
Chris Peakin <christopher.peakin@googlemail.com>
2022-05-04 14:50:08 +0100

    added cors dependency

--- back-end/App.js
+++ back-end/App.js
@@ -6,11 +6,14 @@ const connectDB = require("./src/config/db");
 const quizRoutes = require("./src/routers/quizRouter");
 const userRoutes = require("./src/routers/userRouter");
 const questionRouter = require("./src/routers/questionRouter");
+const cors = require("cors");
 const port = process.env.PORT || 4000;
 
 connectDB()
 
 const app = express();
+
+app.use((cors));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
